05b32a1041d544e61e118d20c994180ec68aa77a
lpakingan <lpak98@gmail.com>
2023-04-16 20:21:28 -1000

    add variable arrays for the different possible character types

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,9 @@
 // Assignment code here
-
+var lowercaseSet = 'abcdefghijklmnopqrstuvwxyz'
+var uppercaseSet = lowercaseSet.toUpperCase().split("")
+lowercaseSet = lowercaseSet.split("")
+var numbersSet = '0123456789'.split("")
+var specialcharactersSet = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split("")
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
